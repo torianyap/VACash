6491847894884ad03794c50f23288aa549a0ba6a
Meily Zanetta <zanettameily@gmail.com>
2023-07-05 16:30:17 +0700

    loginpage1 done

--- app/src/main/java/com/example/vacash/login_page1.java
+++ app/src/main/java/com/example/vacash/login_page1.java
@@ -2,13 +2,29 @@ package com.example.vacash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login_page1 extends AppCompatActivity {
 
+    Button buttonloginpage1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page1);
+        buttonloginpage1 = (Button) findViewById(R.id.button_loginpage1);
+        buttonloginpage1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+    }
+
+    public void openNewActivity(){
+        Intent intent = new Intent(this, LoginPage.class);
+        startActivity(intent);
     }
 }
