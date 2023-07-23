61ccf194f695f8f27bbb55be89fd7db8b43db38d
janettempt <janettemipt@gmail.com>
2023-07-23 16:40:46 +0700

    navbar_itempage

--- app/src/main/java/com/example/vacash/ItemPage.java
+++ app/src/main/java/com/example/vacash/ItemPage.java
@@ -4,13 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.PopupMenu;
 
 import java.util.ArrayList;
 
 public class ItemPage extends AppCompatActivity {
 
     ArrayList<ItemModel> ItemModel = new ArrayList<>();
+    ImageButton homeIcon;
+    ImageButton hamburgerIcon;
+
 
     int[] itemImg = {R.drawable.growtopia, R.drawable.growtopia, R.drawable.growtopia};
     @Override
@@ -23,6 +33,63 @@ public class ItemPage extends AppCompatActivity {
         Game1_RecyclerViewAdapter adapter = new Game1_RecyclerViewAdapter(this, ItemModel);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        homeIcon = findViewById(R.id.home_icon);
+        hamburgerIcon = findViewById(R.id.hamburgerIcon);
+
+        homeIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navigateToHome();
+            }
+        });
+
+        hamburgerIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopupMenu(view);
+            }
+        });
+    }
+
+    private void showPopupMenu(View view) {
+        PopupMenu popupMenu = new PopupMenu(this, view, Gravity.END);
+        popupMenu.getMenuInflater().inflate(R.menu.dropdown_menu, popupMenu.getMenu());
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.menu_profile:
+                        navigateToProfile();
+                        return true;
+                    case R.id.menu_logout:
+                        navigateToLogout();
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+        popupMenu.show();
+
+        // Change background color when the dropdown is shown
+//        mainLayout.setBackgroundColor(Color.parseColor("#800080")); // Purple color with 50% opacity
+    }
+
+    private void navigateToLogout() {
+        Intent intent = new Intent(this, login_page1.class);
+        startActivity(intent);
+    }
+
+    private void navigateToProfile() {
+        Intent intent = new Intent(this, Profile.class);
+        startActivity(intent);
+    }
+
+    private void navigateToHome() {
+        Intent intent = new Intent(this, Homepage.class);
+        startActivity(intent);
     }
 
     private void setUpItemModel(){
