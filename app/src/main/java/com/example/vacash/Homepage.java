810eb41f3f8f6d0bd736bfade341b5aa91017f5c
Meily Zanetta <zanettameily@gmail.com>
2023-07-23 14:22:48 +0700

    loginpage2 done

--- app/src/main/java/com/example/vacash/Homepage.java
+++ app/src/main/java/com/example/vacash/Homepage.java
@@ -2,13 +2,17 @@ package com.example.vacash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class LoginPage extends AppCompatActivity {
+public class Homepage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_page);
+        setContentView(R.layout.homepage);
     }
+
 }
