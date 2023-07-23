a0abd6a74d95dfffcb069c0b59f32261f55d0bff
Meily Zanetta <zanettameily@gmail.com>
2023-07-23 16:29:24 +0700

    carousel done

---
+++ app/src/main/java/com/example/vacash/CarouselAdapter.java
@@ -0,0 +1,69 @@
+package com.example.vacash;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class CarouselAdapter extends PagerAdapter {
+
+    //penampung semua image dan text di carousel
+    int[] images;
+    String[] texts1;
+    String[] texts2;
+    String[] texts3;
+    Context context;
+
+    //constructor
+    public CarouselAdapter(Context context, int[] images, String[] texts1, String[] texts2, String[] texts3){
+        this.images = images;
+        this.texts1 = texts1;
+        this.texts2 = texts2;
+        this.texts3 = texts3;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View view = inflater.inflate(R.layout.carousel_item, container, false);
+
+        //SET IMAGE
+        ImageView carouselImage = view.findViewById(R.id.imageCarousels);
+
+        TextView limitedtime = view.findViewById(R.id.limitedtime);
+        TextView namagame = view.findViewById(R.id.namagame);
+        TextView description = view.findViewById(R.id.description);
+
+        carouselImage.setImageResource(images[position]);
+        limitedtime.setText(texts1[position]);
+        namagame.setText(texts2[position]);
+        description.setText(texts3[position]);
+
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+
+    //berapa banyak image dan text dalam image carousel
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+}
--- app/src/main/java/com/example/vacash/Homepage.java
+++ app/src/main/java/com/example/vacash/Homepage.java
@@ -1,6 +1,7 @@
 package com.example.vacash;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,10 +10,37 @@ import android.widget.Button;
 
 public class Homepage extends AppCompatActivity {
 
+    ViewPager carouselContainer;
+    int[] carouselImages = {R.drawable.pubgwallpaper, R.drawable.rafaelamoba, R.drawable.genshinwall};
+    String[] carouseltext1 = {"LIMITED TIME", "NOW AVAILABLE", "LIMITED TIME"};
+    String[] carouseltext2 = {"PUBG: Playerunknown's Battlegrounds", "Mobile Legends: Bang Bang", "Genshing Impact"};
+    String[] carouseltext3 = {"Top Battle Royale Mobile Game", "Play With The World!", "Top Battle Royale Mobile Game"};
+    CarouselAdapter carouselAdapter;
+    //posisi image sekarang dimana
+    int currImage = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage);
+
+        carouselContainer = findViewById(R.id.carouselContainer);
+
+        //create adapter
+        carouselAdapter = new CarouselAdapter(this, carouselImages, carouseltext1, carouseltext2, carouseltext3);
+        carouselContainer.setAdapter(carouselAdapter);
+
+        imageChange();
+    }
+
+    void imageChange(){
+        carouselContainer.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                currImage = (carouselContainer.getCurrentItem() +1)%carouselImages.length;
+                carouselContainer.setCurrentItem(currImage);
+                imageChange();
+            }
+        }, 5000);
     }
 
 }
