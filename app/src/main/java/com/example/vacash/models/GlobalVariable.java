dcef7506b94fcc153f8a030522c4c1b9ec629af0
janettempt <janettemipt@gmail.com>
2023-07-25 16:19:46 +0700

    GlobalVariable

--- app/src/main/java/com/example/vacash/models/GlobalVariable.java
+++ app/src/main/java/com/example/vacash/models/GlobalVariable.java
@@ -130,5 +130,73 @@ public class GlobalVariable {
         // GTA V
         GameWithItems gta = new GameWithItems("GTA V", "PC", "Rockstar Games", 4.4, R.drawable.gta);
         gta.items.add(new ItemModel("Great White Shark Cash Card", "GGwp Store", "Trusted since 2020", "180000", R.drawable.gta));
+        gta.items.add(new ItemModel("Tiger Shark Cash Card", "GGwp Store", "Trusted since 2020", "155000", R.drawable.gta));
+        gta.items.add(new ItemModel("Whale Shark Cash Card", "HatBox Store", "Trusted since 2019", "66000", R.drawable.gta));
+        gta.items.add(new ItemModel("Red Shark Cash Card", "Asixx Store", "Trusted since 2020", "55000", R.drawable.gta));
+        games.add(gta);
+
+        // CoD
+        GameWithItems cod = new GameWithItems("Call of Duty", "PC", "Activision", 4.3, R.drawable.eldenring);
+        cod.items.add(new ItemModel("6 Bell Bags", "GGwp Store", "Trusted since 2020", "69000", R.drawable.cod));
+        cod.items.add(new ItemModel("45 Bell Bags", "GGwp Store", "Trusted since 2020", "69000", R.drawable.cod));
+        cod.items.add(new ItemModel("90 Bell Bags", "HatBox Store", "Trusted since 2019", "479000", R.drawable.cod));
+        games.add(cod);
+
+        // Valorant
+        GameWithItems valorant = new GameWithItems("Valorant", "PC", "Riot  Games", 4.6, R.drawable.eldenring);
+        valorant.items.add(new ItemModel("1000 Valo Points", "AJP Store", "Trusted since 2018", "155000", R.drawable.valorant));
+        valorant.items.add(new ItemModel("2000 Valo Points", "Luxvry Store", "Trusted since 2018", "298000", R.drawable.valorant));
+        valorant.items.add(new ItemModel("2500 Valo Points", "AJP Store", "Trusted since 2018", "260000", R.drawable.valorant));
+        games.add(valorant);
+
+        // FIFA
+        GameWithItems fifa = new GameWithItems("FIFA 20", "PC", "EA Sports", 4.7, R.drawable.eldenring);
+        fifa.items.add(new ItemModel("Voucher Point 1050", "GGwp Store", "Trusted since 2020", "50000", R.drawable.fifa));
+        fifa.items.add(new ItemModel("Voucher Point 1600", "GGwp Store", "Trusted since 2020", "80000", R.drawable.fifa));
+        fifa.items.add(new ItemModel("Voucher Point 2200", "AJP Store", "Trusted since 2019", "110000", R.drawable.fifa));
+        games.add(fifa);
+
+        // Minecraft
+        GameWithItems minecraft = new GameWithItems("Minecraft", "PC", "Mojang Studios", 4.3, R.drawable.eldenring);
+        minecraft.items.add(new ItemModel("VIP to VIP+", "NTM Store", "Trusted since 2018", "25000", R.drawable.minecraft));
+        minecraft.items.add(new ItemModel("Mystery Box", "NTM Store", "Trusted since 2019", "48000", R.drawable.minecraft));
+        minecraft.items.add(new ItemModel("Gift", "Heyyo Store", "Trusted since 2017", "95000", R.drawable.minecraft));
+        minecraft.items.add(new ItemModel("Booster", "Aloha Store", "Available in 24 hours", "88000", R.drawable.minecraft));
+        games.add(minecraft);
+
+        // Elden Ring
+        GameWithItems eldenring = new GameWithItems("Elden Ring", "Console", "Bandai Namco", 4.5, R.drawable.eldenring);
+        eldenring.items.add(new ItemModel("Flame of the Redmanes Sword", "GGwp Store", "Trusted since 2020", "180000", R.drawable.eldenring));
+        eldenring.items.add(new ItemModel("Radagon Soreseal", "GGwp Store", "Trusted since 2020", "155000", R.drawable.eldenring));
+        eldenring.items.add(new ItemModel("Spirit Calling Bell", "HatBox Store", "Trusted since 2019", "66000", R.drawable.eldenring));
+        games.add(eldenring);
+
+        // Animal
+        GameWithItems animal = new GameWithItems("Animal Crossing", "Console", "Nintendo", 4.4, R.drawable.eldenring);
+        animal.items.add(new ItemModel("6 Bell Bags", "GGwp Store", "Trusted since 2020", "1000", R.drawable.animal));
+        animal.items.add(new ItemModel("45 Bell Bags", "GGwp Store", "Trusted since 2020", "13000", R.drawable.animal));
+        animal.items.add(new ItemModel("90 Bell Bags", "HatBox Store", "Trusted since 2019", "25000", R.drawable.animal));
+        games.add(animal);
+
+        // Pokemon
+        GameWithItems pokemon = new GameWithItems("Pokemon Arceus", "Console", "Nintendon & Pokemon", 4.3, R.drawable.eldenring);
+        animal.items.add(new ItemModel("Pokeball Legendary", "PokiPoki", "Trusted since 2018", "25000", R.drawable.pokemon));
+        animal.items.add(new ItemModel("Pokeball Normal", "LetsBattle", "Trusted since 2019", "48000", R.drawable.pokemon));
+        games.add(pokemon);
+
+        // Hogwarts
+        GameWithItems hogwarts = new GameWithItems("Hogwarts Legacy", "Console", "Warner Bros", 4.6, R.drawable.eldenring);
+        hogwarts.items.add(new ItemModel("Hogwarts Legacy Deluxe", "Lollipop Store", "Trusted since 2018", "220000", R.drawable.hogwarts));
+        hogwarts.items.add(new ItemModel("Hogwarts Legacy", "Lollipop Store", "Trusted since 2018", "100000", R.drawable.hogwarts));
+        hogwarts.items.add(new ItemModel("Hogwarts Legacy Special", "Day Dream Store", "New", "150000", R.drawable.hogwarts));
+        games.add(hogwarts);
+
+        // Sims
+        GameWithItems sims = new GameWithItems("The Sims 4", "Console", "Electronic Arts", 4.3, R.drawable.eldenring);
+        sims.items.add(new ItemModel("6 Bell Bags", "GGwp Store", "Trusted since 2020", "69000", R.drawable.sims));
+        sims.items.add(new ItemModel("45 Bell Bags", "GGwp Store", "Trusted since 2020", "69000", R.drawable.sims));
+        sims.items.add(new ItemModel("90 Bell Bags", "HatBox Store", "Trusted since 2019", "479000", R.drawable.sims));
+        games.add(sims);
+
     }
 }
